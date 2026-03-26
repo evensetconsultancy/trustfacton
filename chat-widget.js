642d00f593f49c380e78030e9d57a681f5f2a232
evensetconsultancy <admin@evensetconsultancy.com>
2026-03-26 18:33:14 +0530

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -75,7 +75,7 @@ class TrustFactONChat {
     const typingEl = this.showTyping();
 
     try {
-      const response = await fetch(`${BACKEND_URL}/chat`, {
+      const response = await fetch(`${BACKEND_URL}/api/chat`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
